54c79c84d1122a59930fa9a3140e6805503c06d0
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-09-27 09:51:19 -0400

    added 2 contexts, not tested

--- src/context/DataProvider.jsx
+++ src/context/DataProvider.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
+const DataContext = React.createContext();
+
 export default function DataProvider(props) {
-    return (
-        <div>data provider</div>
-    )
+    const [data, setData] = React.useState({});
+    return <DataContext.Provider values={data, setData} {...props}/>
 }
 
 export function useDataProvider() {
-    return (
-        <div>data provider hook</div>
-    )
+    const data = React.useContext(DataContext);
+
+    if (!data) {
+        throw new Error('useDataProvider must be used inside DataProvider.')
+    }
+
+    return data;
 }
--- src/context/ThemeProvider.jsx
+++ src/context/ThemeProvider.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
+const ThemeContext = React.createContext();
+
 export default function ThemeProvider(props) {
-    return (
-        <div>theme provider</div>
-    )
+    const [theme, setTheme] = React.useState('light');
+
+    return <ThemeContext.Provider values={theme, setTheme} {...props} />
 }
 
 export function useThemeProvider() {
-    return (
-        <div>use theme provider</div>
-    )
+    const theme = React.useContext(ThemeContext);
+
+    if (!theme) {
+        throw new Error('useThemeProvider must be used inside ThemeProvider')
+    }
+
+    return theme;
 }
