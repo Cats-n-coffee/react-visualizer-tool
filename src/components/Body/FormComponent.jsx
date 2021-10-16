93a04bc378bb11ce5ab9c81e50229a553d610b17
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-10-15 22:33:50 -0400

    added parent list in dropdown

--- src/components/Body/FormComponent.jsx
+++ src/components/Body/FormComponent.jsx
@@ -1,168 +1,208 @@
-import React from 'react';
-import { Formik, Field, Form, FieldArray, ErrorMessage } from 'formik';
-import { findNodeAndRead, nodeNameList } from '../../helpers/findNodeInTree';
+import React from "react";
+import { Formik, Field, Form, FieldArray, ErrorMessage } from "formik";
+import { findNodeAndRead, nodeNameList } from "../../helpers/findNodeInTree";
 
 export default function FormComponent(props) {
-    const { handleSubmitNew, handleSubmitEdit, type, componentToEdit = {}, data } = props;
+  const {
+    handleSubmitNew,
+    handleSubmitEdit,
+    type,
+    componentToEdit = {},
+    data,
+  } = props;
 
-    const initialValues = {
-        name: componentToEdit?.name || "",
-        props: componentToEdit?.props || [],
-        state: componentToEdit?.state || [],
-        parent: componentToEdit?.parent || ""
+  const initialValues = {
+    name: componentToEdit?.name || "",
+    props: componentToEdit?.props || [],
+    state: componentToEdit?.state || [],
+    parent: componentToEdit?.parent || "",
+  };
+
+  const nameBeforeEdit = componentToEdit?.name || "";
+
+  if (type === "edit") {
+    console.log("%cpopup form received", "color: purple", componentToEdit);
+  }
+  const nodeList = nodeNameList(data[0], []);
+
+  console.log(
+    "%cnodeNameList is ",
+    "color: orange",
+    nodeList,
+    "current data ",
+    data[0]
+  );
+
+  function validateName(name) {
+    if (type === "edit" && name === initialValues.name) return;
+    const checkName = findNodeAndRead(name, data[0]);
+    console.log("%cinside the validate", "color: red", checkName);
+    if (checkName === "No match" || checkName === "No component yet") {
+      console.log("%cAll good", "color: red");
+      return null;
+    } else {
+      console.log("%cComponent name already used", "color: red", checkName);
+      return "Component name already used";
     }
+  }
 
-    const nameBeforeEdit = componentToEdit?.name || "";
-
-    if (type === 'edit') {
-        console.log('%cpopup form received', 'color: purple', componentToEdit)
-    }
-    //const nameList = []
-    const nodeList = nodeNameList(data[0], [])
-
-    console.log('%cnodeNameList is ', 'color: orange', nodeList, 'current data ', data[0])
-
-    function validateName(name) {
-        if (type === 'edit' && name === initialValues.name) return;
-        const checkName = findNodeAndRead(name, data[0]);
-        console.log('%cinside the validate', 'color: red', checkName)
-        if ((checkName === 'No match') || (checkName === 'No component yet')) {
-            console.log('%cAll good', 'color: red')
-            return null;
-        }
-        else {
-            console.log('%cComponent name already used', 'color: red', checkName)
-            return 'Component name already used';
-        }
-    }
-
-    return (
-        <section>
-            <h2>{ type }</h2>
-            <Formik
-                initialValues={ initialValues }
-                validateOnBlur={ true }
-                validateOnChange={ false }
-                enableReinitialize={ true }
-                onSubmit={(data, { setSubmitting }) => {
-                    console.log('submitting', data)
-                    if (type === 'new') {
-                        handleSubmitNew(data)
-                    }
-                    else if (type === 'edit') {
-                        handleSubmitEdit(nameBeforeEdit, data)
-                    }
-                }}
-            >
-                {
-                    ({values, isSubmitting, errors}) => (
-                        <Form>
-                            <fieldset>
-                                <label htmlFor="component-name">Component Name</label>
-                                <Field 
-                                type="text" 
-                                id="component-name" 
-                                name="name" 
-                                validate={ validateName }
-                                />
-                                {errors.name && <div>{ errors.name }</div>}
-                            </fieldset>
-                            <fieldset>
-                                <h3>Component Props:</h3>
-                                <FieldArray name="props">
-                                {
-                                    (arrayHelpers) => (
-                                        <>
-                                            {
-                                                values.props.map((prop, index) => {
-                                                return (
-                                                    <div key={`props.${index}`}>
-                                                        <div>
-                                                            <label htmlFor={`props.${index}.propName`}>Prop Name</label>
-                                                            <Field type="text" id={`props.${index}.propName`} name={`props.${index}.propName`}/>
-                                                        </div>
-                                                        <div>
-                                                            <label htmlFor={`props.${index}.propValue`}>Prop Value</label>
-                                                            <Field type="text" id={`props.${index}.propValue`} name={`props.${index}.propValue`}/> 
-                                                        </div>
-                                                        <button
-                                                            onClick={() => arrayHelpers.remove(index)}
-                                                            type="button"
-                                                        >
-                                                            Delete
-                                                        </button>
-                                                    </div>
-                                                )
-                                                })
-                                            }
-                                            <button
-                                                type="button"
-                                                onClick={() => arrayHelpers.push({propName: "", propValue: ""})}
-                                            >
-                                                Add prop
-                                            </button>
-                                        </>
-                                    )
-                                }
-                                </FieldArray>
-                            </fieldset>
-                            <fieldset>
-                                <h3>State:</h3>
-                                <FieldArray name="state">
-                                {
-                                    (arrayHelpers) => (
-                                        <>
-                                            {
-                                                values.state.map((item, index) => {
-                                                return (
-                                                    <div key={`state.${index}`}>
-                                                        <div>
-                                                            <label htmlFor={`state.${index}.stateName`}>State Name</label>
-                                                            <Field type="text" id={`state.${index}.stateName`} name={`state.${index}.stateName`}/>
-                                                        </div>
-                                                        <div>
-                                                            <label htmlFor={`state.${index}.stateValue`}>State Value</label>
-                                                            <Field type="text" id={`state.${index}.stateValue`} name={`state.${index}.stateHook`}/> 
-                                                        </div>
-                                                        <button
-                                                            onClick={() => arrayHelpers.remove(index)}
-                                                            type="button"
-                                                        >
-                                                            Delete
-                                                        </button>
-                                                    </div>
-                                                )
-                                                })
-                                            }
-                                            <button
-                                                type="button"
-                                                onClick={() => arrayHelpers.push({stateName: "", stateHook: ""})}
-                                            >
-                                                Add state
-                                            </button>
-                                        </>
-                                    )
-                                }
-                                </FieldArray>
-                            </fieldset>
-                            <fieldset>
-                                <label htmlFor="component-parent">Component Parent</label>
-                                {
-                                    type === 'new' ?
-                                    <Field type="text" id="component-parent" name="parent"/>
-                                    : <Field type="text" id="component-parent" name="parent" disabled/>
-                                }
-                                
-                            </fieldset>
-                            {
-                                type === 'new' ?
-                                <button type="submit" >Add to Tree</button>
-                                : <button type="submit" >Edit Tree</button>
-                            }
-                        </Form>
-                    )
-                }
-            </Formik>
-        </section>
-    )
+  return (
+    <section>
+      <h2>{type}</h2>
+      <Formik
+        initialValues={initialValues}
+        validateOnBlur={true}
+        validateOnChange={false}
+        enableReinitialize={true}
+        onSubmit={(data, { setSubmitting }) => {
+          console.log("submitting", data);
+          if (type === "new") {
+            handleSubmitNew(data);
+          } else if (type === "edit") {
+            handleSubmitEdit(nameBeforeEdit, data);
+          }
+        }}
+      >
+        {({ values, isSubmitting, errors }) => (
+          <Form>
+            <fieldset>
+              <label htmlFor="component-name">Component Name</label>
+              <Field
+                type="text"
+                id="component-name"
+                name="name"
+                validate={validateName}
+              />
+              {errors.name && <div>{errors.name}</div>}
+            </fieldset>
+            <fieldset>
+              <h3>Component Props:</h3>
+              <FieldArray name="props">
+                {(arrayHelpers) => (
+                  <>
+                    {values.props.map((prop, index) => {
+                      return (
+                        <div key={`props.${index}`}>
+                          <div>
+                            <label htmlFor={`props.${index}.propName`}>
+                              Prop Name
+                            </label>
+                            <Field
+                              type="text"
+                              id={`props.${index}.propName`}
+                              name={`props.${index}.propName`}
+                            />
+                          </div>
+                          <div>
+                            <label htmlFor={`props.${index}.propValue`}>
+                              Prop Value
+                            </label>
+                            <Field
+                              type="text"
+                              id={`props.${index}.propValue`}
+                              name={`props.${index}.propValue`}
+                            />
+                          </div>
+                          <button
+                            onClick={() => arrayHelpers.remove(index)}
+                            type="button"
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      );
+                    })}
+                    <button
+                      type="button"
+                      onClick={() =>
+                        arrayHelpers.push({ propName: "", propValue: "" })
+                      }
+                    >
+                      Add prop
+                    </button>
+                  </>
+                )}
+              </FieldArray>
+            </fieldset>
+            <fieldset>
+              <h3>State:</h3>
+              <FieldArray name="state">
+                {(arrayHelpers) => (
+                  <>
+                    {values.state.map((item, index) => {
+                      return (
+                        <div key={`state.${index}`}>
+                          <div>
+                            <label htmlFor={`state.${index}.stateName`}>
+                              State Name
+                            </label>
+                            <Field
+                              type="text"
+                              id={`state.${index}.stateName`}
+                              name={`state.${index}.stateName`}
+                            />
+                          </div>
+                          <div>
+                            <label htmlFor={`state.${index}.stateValue`}>
+                              State Value
+                            </label>
+                            <Field
+                              type="text"
+                              id={`state.${index}.stateValue`}
+                              name={`state.${index}.stateHook`}
+                            />
+                          </div>
+                          <button
+                            onClick={() => arrayHelpers.remove(index)}
+                            type="button"
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      );
+                    })}
+                    <button
+                      type="button"
+                      onClick={() =>
+                        arrayHelpers.push({ stateName: "", stateHook: "" })
+                      }
+                    >
+                      Add state
+                    </button>
+                  </>
+                )}
+              </FieldArray>
+            </fieldset>
+            <fieldset>
+              <label htmlFor="component-parent">Component Parent</label>
+              {type === "new" ? (
+                <Field as="select" id="component-parent" name="parent">
+                  <option>Select a Parent</option>
+                  {nodeList && nodeList.length
+                    ? nodeList.map((node, index) => (
+                        <option value={node} key={`${node}-index${index}`}>
+                          {node}
+                        </option>
+                      ))
+                    : null}
+                </Field>
+              ) : (
+                <Field
+                  as="select"
+                  id="component-parent"
+                  name="parent"
+                  disabled
+                />
+              )}
+            </fieldset>
+            {type === "new" ? (
+              <button type="submit">Add to Tree</button>
+            ) : (
+              <button type="submit">Edit Tree</button>
+            )}
+          </Form>
+        )}
+      </Formik>
+    </section>
+  );
 }
--- src/helpers/findNodeInTree.js
+++ src/helpers/findNodeInTree.js
@@ -1,90 +1,94 @@
 export function findNodeAndInsert(nodeName, childToInsert, parentNode) {
-    if (parentNode.name === nodeName) {
-        parentNode.allChildren.push(childToInsert);
+  if (parentNode.name === nodeName) {
+    parentNode.allChildren.push(childToInsert);
+  } else {
+    for (let i = 0; i < parentNode.allChildren.length; i += 1) {
+      findNodeAndInsert(nodeName, childToInsert, parentNode.allChildren[i]);
     }
-    else {
-        for (let i = 0; i < parentNode.allChildren.length; i += 1) {
-            findNodeAndInsert(nodeName, childToInsert, parentNode.allChildren[i]);
-        }
-    }
-    return [parentNode];
+  }
+  return [parentNode];
 }
 
 export function findNodeAndDelete(nodeName, parentNode) {
-    // removing the root node will remove all the children, so we start over
-    console.log('delete nodeName', nodeName, 'parentNode', parentNode);
-    if (parentNode.parent === null && parentNode.name === nodeName) {
-        console.log('parent node deleting')
-        return [];
+  // removing the root node will remove all the children, so we start over
+  console.log("delete nodeName", nodeName, "parentNode", parentNode);
+  if (parentNode.parent === null && parentNode.name === nodeName) {
+    console.log("parent node deleting");
+    return [];
+  }
+  // we need to inspect inside the allChildren array to find the correct node
+  else {
+    console.log("deleting some other node");
+    for (let i = 0; i < parentNode.allChildren.length; i += 1) {
+      if (parentNode.allChildren[i].name === nodeName) {
+        parentNode.allChildren.splice(i, 1);
+      } else {
+        findNodeAndDelete(nodeName, parentNode.allChildren[i]);
+      }
     }
-    // we need to inspect inside the allChildren array to find the correct node
-    else {
-        console.log('deleting some other node')
-        for (let i = 0; i < parentNode.allChildren.length; i += 1) {
-            if (parentNode.allChildren[i].name === nodeName) {
-                parentNode.allChildren.splice(i, 1);
-            }
-            else {
-                findNodeAndDelete(nodeName, parentNode.allChildren[i]);
-            }
-        }
-    }
-    return [parentNode];
+  }
+  return [parentNode];
 }
 
 export function findNodeAndRead(nodeName, parentNode) {
-    console.log('%cnodeName', 'color: green', nodeName, 'parentNode', parentNode)
-    if (!parentNode) return 'No component yet';
-    if (parentNode.name === nodeName) { 
-        return parentNode;
+  console.log("%cnodeName", "color: green", nodeName, "parentNode", parentNode);
+  if (!parentNode) return "No component yet";
+  if (parentNode.name === nodeName) {
+    return parentNode;
+  } else if (
+    parentNode.name !== nodeName &&
+    (!parentNode.allChildren || parentNode.allChildren.length === 0)
+  ) {
+    return "No match";
+  } else {
+    for (let i = 0; i < parentNode.allChildren.length; i += 1) {
+      return findNodeAndRead(nodeName, parentNode.allChildren[i]);
     }
-    else if (parentNode.name !== nodeName && (!parentNode.allChildren || parentNode.allChildren.length === 0)) {
-        return "No match";
-    }
-    else {
-        for (let i = 0; i < parentNode.allChildren.length; i += 1) {
-            return findNodeAndRead(nodeName, parentNode.allChildren[i]);
-        }
-    }
+  }
 }
 
 export function findNodeAndUpdate(nodeName, editedNode, parentNode) {
-    console.log('%cinside findandUpdate', 'color: purple', nodeName, 'edit', editedNode, 'parent', parentNode);
-    if (parentNode.name === nodeName) {
-        parentNode.name = editedNode.name;
-        parentNode.props = editedNode.props;
-        parentNode.state = editedNode.state;
-        
-        // we need to update the parent property of all the children in the array
-        for (let i = 0; i < parentNode.allChildren.length; i += 1) {
-            let child = parentNode.allChildren[i];
-            child.parent = editedNode.name;
-        }
+  console.log(
+    "%cinside findandUpdate",
+    "color: purple",
+    nodeName,
+    "edit",
+    editedNode,
+    "parent",
+    parentNode
+  );
+  if (parentNode.name === nodeName) {
+    parentNode.name = editedNode.name;
+    parentNode.props = editedNode.props;
+    parentNode.state = editedNode.state;
+
+    // we need to update the parent property of all the children in the array
+    for (let i = 0; i < parentNode.allChildren.length; i += 1) {
+      let child = parentNode.allChildren[i];
+      child.parent = editedNode.name;
     }
-    else {
-        for (let i = 0; i < parentNode.allChildren.length; i += 1) {
-            findNodeAndUpdate(nodeName, editedNode, parentNode.allChildren[i]);
-        }
+  } else {
+    for (let i = 0; i < parentNode.allChildren.length; i += 1) {
+      findNodeAndUpdate(nodeName, editedNode, parentNode.allChildren[i]);
     }
-    return [parentNode];
+  }
+  return [parentNode];
 }
 
-
 export function nodeNameList(parent, nameList) {
-    if (!parent) return null;
-    else if (parent.parent === null && parent.allChildren.length === 0) {
-        // handles the case where we only have a root node
-        nameList.push(parent.name);
+  if (!parent) return null;
+  else if (parent.parent === null && parent.allChildren.length === 0) {
+    // handles the case where we only have a root node
+    nameList.push(parent.name);
+  } else if (parent.allChildren && parent.allChildren.length > 0) {
+    // handles the case where the root node has children
+    if (nameList.indexOf(parent.name) === -1) {
+      nameList.push(parent.name);
     }
-    else if (parent.allChildren && parent.allChildren.length > 0) {
-        // handles the case where the root node has children
-        if (nameList.indexOf(parent.name) === -1) {
-            nameList.push(parent.name); 
-        }
-        for (let i = 0; i < parent.allChildren.length; i += 1) {
-            nameList.push(parent.allChildren[i].name);
-            nodeNameList(parent.allChildren[i], nameList)
-        }
+    for (let i = 0; i < parent.allChildren.length; i += 1) {
+      nameList.push(parent.allChildren[i].name);
+      nodeNameList(parent.allChildren[i], nameList);
     }
-    return nameList;
+  }
+  return nameList;
 }
